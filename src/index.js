b99ae23ef79a56e00f5fd91683361b102a80ca56
Ssiyam0123 <ssiyam563@gmail.com>
2025-12-17 23:31:12 +0600

    cloudinary added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import express from "express";
 import "dotenv/config";
 import authRoute from "./routes/authRoutes.js";
+import bookRoute from "./routes/bookRoutes.js"
 import { connectDb } from "./lib/db.js";
+import cors from 'cors'
+import cookieParser from "cookie-parser"
 
 const app = express();
 
-/* ✅ MUST be before routes */
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/auth", authRoute);
+app.use("/api/books", bookRoute);
+
+
 
 const PORT = process.env.PORT || 3000;
 
